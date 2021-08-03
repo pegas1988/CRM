83cd5c9fcb54f46913f670e344a83449cda1cea7
Mike <pegas19881@gmail.com>
2021-08-03 23:36:58 +0300

    small changes

--- src/main/java/com/hillel/crm/config/controller/LoginController.java
+++ src/main/java/com/hillel/crm/config/controller/LoginController.java
@@ -4,15 +4,14 @@ import com.hillel.crm.config.entity.Button;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
-@RequestMapping("      /loginMain")
-public class LoginController{
-
-   // private UserService userService = new UserService();
+@RestController
+@RequestMapping(value = "/loginMain")
+public class LoginController {
 
     private String createButton(Button button, String buttonName) {
         button.setButtonForNavbar(buttonName);
@@ -23,42 +22,4 @@ public class LoginController{
     public String getSomeString2() {
         return "login.jsp";
     }
-
-    public ControllerResultDto execute(HttpServletRequest req, HttpServletResponse resp) {
-//        Button button = new Button();
-//        String userName = req.getParameter("firstName");
-//        String userLastName = req.getParameter("lastName");
-//        String password = req.getParameter("psw");
-//        User userFind = User.builder()
-//                .firstName(userName)
-//                .lastName(userLastName)
-//                .build();
-////        User user = userService.findByFirstAndLastName(userFind);
-////        if (user.getPassword().equals(password)) {
-////            //req.setAttribute("button", button);
-////            req.setAttribute("user", user);
-////
-////            switch (user.getUserRole()) {
-////                case DESIGNER:
-////                    req.getSession().setAttribute("UserId", user.getUserID());
-////                    return new ControllerResultDto("designer");
-////                case DOCTOR:
-////                    req.getSession().setAttribute("UserId", user.getUserID());
-////                    return new ControllerResultDto("doctor");
-////                case ADMIN: {
-////                    req.getSession().setAttribute("UserId", user.getUserID());
-////                    req.getSession().setAttribute("button", createButton(button, "admin"));
-////                    return new ControllerResultDto("admin");
-////                }
-////                case ADMINISTRATOR: {
-////                    req.getSession().setAttribute("UserId", user.getUserID());
-////                    req.getSession().setAttribute("button", createButton(button, "administrator"));
-////                    return new ControllerResultDto("administrator");
-////                }
-////                default:
-////                    return new ControllerResultDto("error-403");
-////            }
-////        } else
-        return new ControllerResultDto("error-403");
-         }
 }
